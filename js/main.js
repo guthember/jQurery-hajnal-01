e6201af9b9ec01323ea84519ed4452d98a37be59
guthember <guthember@gmail.com>
2018-07-21 16:58:03 +0200

    2nd day end

--- js/main.js
+++ js/main.js
@@ -8,24 +8,25 @@ $(document).ready(function() {
   $("p").click(function(event) {
     // event.preventDefault();
     // $(this).hide();
-    $(this).hide();
     // $(this).fadeTo(5000, 1, fadeDone);
-    $(this).slideDown(3500);
+    // $(this).slideDown(3500).css("color","blue");
   });
 
   // esemény kiváltása
-  $("p").click();
+  // $("p").click();
 
   $("nav a.nav-link").click( function (ev) {
     // kattintás megelőzése
     ev.preventDefault();
     // console.log(ev);
-    var link = $(this);
-    var prop = link.data("prop") || "opacity";
-    var val = link.data("value") || "0";
-    var speed = link.data("speed") || 3000;
-    var settings = {};
-    settings[prop] = val;
+
+    startPage(this, 1, false);
+    // var link = $(this);
+    // var prop = link.data("prop") || "opacity";
+    // var val = link.data("value") || "0";
+    // var speed = link.data("speed") || 3000;
+    // var settings = {};
+    // settings[prop] = val;
 
     // paraméter nélkül
     // $(document.body).animate({
@@ -35,8 +36,48 @@ $(document).ready(function() {
     // });
 
     // paraméterrel
-    $(document.body).animate( settings, speed, function(){
-      document.location = link.attr("href");
-    });
+    // $(document.body).animate( settings, speed, function(){
+    //   document.location = link.attr("href");
+    // });
   });
+});
+
+function startPage( elem, num, bool) {
+  var link = $(elem);
+  var prop = link.data("prop") || "opacity";
+  var val = link.data("value") || "0";
+  var speed = link.data("speed") || 3000;
+  var settings = {};
+  settings[prop] = val;
+  // console.log("num: ",num);
+  // console.log("bool: ",bool);
+  $(document.body).animate( settings, speed, function(){
+    document.location = link.attr("href");
+  });
+};
+
+// events.html
+$(".events-search-row input").on("keyup", function(ev) {
+$.each( $(".events-card-deck .card .card-title"), function(i, e)   {
+    var elem = $(e);
+    var search = ev.target.value.toLowerCase();
+    var content = elem.html().toLowerCase();
+    if ( content.indexOf(search) == -1 ) {
+      elem.parents(".card").hide();
+    } else {
+      elem.parents(".card").show();
+    }
+  });
+});
+
+// register.html
+$(".cherry-custom-file").on("change", function(ev) {
+  var name = ev.target.value.split("\\").pop();
+  $(this).find(".file-name").html(
+    name);
+});
+
+// ticket.html
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
